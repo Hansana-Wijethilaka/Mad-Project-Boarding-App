47927cc996f574231d45c3690779aa9ec025f473
RusiraPathum <rusira.pathum20@gmail.com>
2020-04-14 23:16:44 +0530

    hosat view part

--- app/src/main/java/com/example/boadme/Hostal_listviewActivity.java
+++ app/src/main/java/com/example/boadme/Hostal_listviewActivity.java
@@ -10,6 +10,9 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hostal_listviewActivity extends AppCompatActivity {
 
     private Button add;
@@ -17,6 +20,7 @@ public class Hostal_listviewActivity extends AppCompatActivity {
     private TextView count;
     Context context;
     private DbHandler dbHandler;
+    private List<Hostal> hostalList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +30,16 @@ public class Hostal_listviewActivity extends AppCompatActivity {
         context = this;
         dbHandler = new DbHandler(context);
         add = findViewById(R.id.add);
+
         listview = findViewById(R.id.hostal_list);
         count = findViewById(R.id.hostal_count);
+
+        hostalList = new ArrayList<>();
+        hostalList = dbHandler.getAllHostal();
+
+        HostalAdapter hostalAdapter = new HostalAdapter(context,R.layout.singale_hostal,hostalList);
+
+        listview.setAdapter(hostalAdapter);
 
         int count_Hostal = dbHandler.countHostal();
         count.setText("You have "+count_Hostal+ " Hostal");
